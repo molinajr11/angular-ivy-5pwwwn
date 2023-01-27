6571a91e1f8d57369ae56765ff7e403ecd1e8924
molinajr11 <105083946+molinajr11@users.noreply.github.com>
2023-01-26 19:09:46 -0500

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
+import { ProductosModule } from './productos/productos.module';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => AuthModule),
+  },
+  {
+    path: 'productos',
+    loadChildren: () =>
+      import('./productos/productos.module').then((m) => ProductosModule),
   },
   { path: '**', redirectTo: 'auth' },
 ];
